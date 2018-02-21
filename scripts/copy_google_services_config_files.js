d09fb83bb04b8a1117be43f5b03cce5aa5cbf1ab
André Vieira <andre.vieira@outsystems.com>
2018-02-21 15:12:45 +0000

    Update copy_google_services_config_files.js

--- scripts/copy_google_services_config_files.js
+++ scripts/copy_google_services_config_files.js
@@ -30,6 +30,7 @@ var ANDROID_DIR = 'platforms/android';
 var PLATFORM = {
     IOS: {
         dest: [
+            IOS_DIR + '/GoogleService-Info.plist',
             IOS_DIR + '/' + name + '/Resources/GoogleService-Info.plist',
             IOS_DIR + '/' + name + '/Resources/Resources/GoogleService-Info.plist'
         ],
